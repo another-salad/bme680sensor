239b92f776c59bb315bdf20f1db8dbfd71a303b2
jumbobumbo <dwheels91@gmail.com>
2022-02-28 23:54:52 +0000

    sends temp data on inital connect

--- app/main_api.py
+++ app/main_api.py
@@ -4,10 +4,17 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
 from common.config_reader import get_conf
+from common.bme_sensor import BMESensor
 
-app_config = get_conf()
+# config for socket app and BME680 sensor
+config = get_conf()
+
+# BME680 Sensor variable in waiting...
+bme_sensor = None
+
+# Flask app
 app = Flask(__name__)
-app.config['SECRET_KEY'] = app_config.SECRET_KEY
+app.config['SECRET_KEY'] = config.SECRET_KEY
 socketio = SocketIO(app)
 
 @app.route('/')
@@ -15,12 +22,17 @@ def index():
     return render_template('index.html', async_mode=socketio.async_mode)
 
 @socketio.on('connect')
-def test_connect(auth):
-    emit('my response', {'data': 'Connected'})
+def connect(auth):
+    emit('connect', {'data': 'Connected'})
+
+@socketio.on('initialConnect')
+def populate_all():
+    emit('setTemp', bme_sensor.temperature)
 
 @socketio.on('disconnect')
 def test_disconnect():
     print('Client disconnected')
 
 if __name__ == '__main__':
+    bme_sensor = BMESensor(sea_level_pressure=config)
     socketio.run(app, host="0.0.0.0")
