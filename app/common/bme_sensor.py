af1b9d839d69b74d35779f5706887a6286680d0f
jumbobumbo <dwheels91@gmail.com>
2022-03-02 14:14:42 +0000

    still very WIP

--- app/common/bme_sensor.py
+++ app/common/bme_sensor.py
@@ -6,6 +6,7 @@ from  adafruit_bme680 import Adafruit_BME680_I2C
 from config_reader import get_conf
 
 
+# BME Sensor object
 class BMESensor(Adafruit_BME680_I2C):
     """BME Sensor wrapper"""
 
@@ -13,3 +14,32 @@ class BMESensor(Adafruit_BME680_I2C):
         """init baby"""
         super().__init__(i2c=i2c)
         self.sea_level_pressure = sea_level_pressure.seaLevelhPa
+
+
+# BME680 Sensor data
+class BMESensorProperties(BMESensor):
+    """Gets the sensor data for the socket emits"""
+
+    def __init__(self, sea_level_pressure):
+        """init baby"""
+        super().__init__(sea_level_pressure=sea_level_pressure)
+
+    @property
+    def getTemp(self) -> tuple:
+        """get temp"""
+        return 'getTemp', round(self.temperature, 2)
+
+    @property
+    def getHumidity(self) -> tuple:
+        """get humidity"""
+        return 'getHumidity', round(self.humidity, 2)
+
+    @property
+    def getAirQuality(self) -> tuple:
+        """get AirQuality"""
+        return 'getAirQuality', self.gas
+
+    @property
+    def getPressure(self) -> tuple:
+        """get pressure"""
+        return 'getPressure', round(self.pressure, 2)
--- app/main_api.py
+++ app/main_api.py
@@ -1,38 +1,56 @@
 """main"""
 
+from time import sleep
+
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
 from common.config_reader import get_conf
-from common.bme_sensor import BMESensor
+from common.bme_sensor import BMESensorProperties
+
 
 # config for socket app and BME680 sensor
-config = get_conf()
-
-# BME680 Sensor variable in waiting...
-bme_sensor = None
+CONFIG = get_conf()
 
 # Flask app
 app = Flask(__name__)
-app.config['SECRET_KEY'] = config.SECRET_KEY
+app.config['SECRET_KEY'] = CONFIG.SECRET_KEY
 socketio = SocketIO(app)
+
+class Emitter:
+    """"""
+
+    def __init__(self, watch_obj):
+        """"""
+        self.watch_obj = watch_obj
+
+    def update(self, attrib):
+        """"""
+        if hasattr(self.watch_obj, attrib):
+            property, value = getattr(self.watch_obj, attrib)
+            emit(property, value)
+
+# BME Sensor
+bme_sensor = Emitter(BMESensorProperties(CONFIG))
+
+def _set_sensor_values(bme_sensor):
+    for attrib in dir(bme_sensor.watch_obj):
+        if attrib.startswith('get'):
+            bme_sensor.update(attrib)
+    
+    emit('query')
 
 @app.route('/')
 def index():
     return render_template('index.html', async_mode=socketio.async_mode)
 
-@socketio.on('connect')
-def connect(auth):
-    emit('connect', {'data': 'Connected'})
-
-@socketio.on('initialConnect')
-def populate_all():
-    emit('setTemp', bme_sensor.temperature)
+@socketio.on('query')
+def query():
+    _set_sensor_values(bme_sensor)
 
 @socketio.on('disconnect')
-def test_disconnect():
-    print('Client disconnected')
+def disconnect():
+    pass
 
 if __name__ == '__main__':
-    bme_sensor = BMESensor(sea_level_pressure=config)
     socketio.run(app, host="0.0.0.0")
