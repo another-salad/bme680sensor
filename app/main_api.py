78f741d6d08cd1a1e5e6876c22efedc733585dcc
jumbobumbo <dwheels91@gmail.com>
2022-03-02 21:04:02 +0000

    requirements fix, comments

--- app/main_api.py
+++ app/main_api.py
@@ -18,14 +18,14 @@ app.config['SECRET_KEY'] = CONFIG.SECRET_KEY
 socketio = SocketIO(app)
 
 class Emitter:
-    """"""
+    """Emits the sensor properties to a connected client"""
 
     def __init__(self, watch_obj):
-        """"""
+        """init baby"""
         self.watch_obj = watch_obj
 
     def update(self, attrib):
-        """"""
+        """emits updated value for attrib"""
         if hasattr(self.watch_obj, attrib):
             property, value = getattr(self.watch_obj, attrib)
             emit(property, value)
@@ -34,6 +34,8 @@ class Emitter:
 bme_sensor = Emitter(BMESensorProperties(CONFIG))
 
 def _set_sensor_values(bme_sensor):
+    """Sends the sensor values to the client via the Emitter, then emits query so the
+    client asks for an update."""
     for attrib in dir(bme_sensor.watch_obj):
         if attrib.startswith('get'):
             bme_sensor.update(attrib)
@@ -42,14 +44,17 @@ def _set_sensor_values(bme_sensor):
 
 @app.route('/')
 def index():
+    """Main html page"""
     return render_template('index.html', async_mode=socketio.async_mode)
 
 @socketio.on('query')
 def query():
+    """Updates sensor values"""
     _set_sensor_values(bme_sensor)
 
 @socketio.on('disconnect')
 def disconnect():
+    """This currently does nothing"""
     pass
 
 if __name__ == '__main__':
